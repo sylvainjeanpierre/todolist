910a7ec1701c8307a217cc3370483d5f5c542788
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2023-01-26 19:48:00 +0100

    setup database

--- app/controller/Controller.php
+++ app/controller/Controller.php
@@ -6,6 +6,7 @@ class Controller
     public static function home()
     {
         include 'config.php';
+        $results = Model::getTasks();
         $view =  $root . 'app/view/home.php';
         require($view);
     }
--- app/model/Model.php
+++ app/model/Model.php
@@ -4,5 +4,13 @@ require_once 'ModelDB.php';
 
 
 class Model {
-    
+    public static function getTasks() {
+        $db = ModelDB::getInstance();
+        $query = "SELECT * FROM tasks";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $results = $statement->fetchAll();
+        $statement->closeCursor();
+        return $results;
+    }
 }
