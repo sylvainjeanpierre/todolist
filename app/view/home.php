2ddb8a105b665f703d23a7466a5dcafb05b4ca08
sylvainjeanpierre <112515717+sylvainjeanpierre@users.noreply.github.com>
2023-01-31 16:46:16 +0100

    Change body style

--- app/view/home.php
+++ app/view/home.php
@@ -1,22 +1,22 @@
 <?php include 'header.html'; ?>
 
 <div class="base">
-    <h1>Todo List</h1>
+    <h1>To-do list</h1>
 
     <h2>New task</h2>
-    <form action="router.php" method="get">
+    <form action="router.php" method="get" class="new_task_form">
         <label for="new_task"></label>
         <input type="hidden" name="action" value="addTask">
-        <input type="text" id="new_task" name="content"><br>
+        <input type="text" id="new_task" name="content">
         <input type="submit" value="Insert">
     </form>
 
 
-    <h3>Tasks : </h3>
+    <h3>Tasks</h3>
 
     <ul>
         <?php foreach ($results as $result) { ?>
-            <li>
+            <li class="tasks_line">
                 <?= $result['content']; ?>
                 <form action="router.php" method="get">
                     <input type="hidden" name="action" value="deleteTask">
